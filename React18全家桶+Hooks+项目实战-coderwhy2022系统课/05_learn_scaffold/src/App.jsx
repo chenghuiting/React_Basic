76a1b88dee1b241daaf63daa0b5512b0bbe5a45b
“chenghuiting” <1365256914@qq.com>
2024-05-30 21:30:37 +0800

    css transition动画

--- React18全家桶+Hooks+项目实战-coderwhy2022系统课/05_learn_scaffold/src/App.jsx
+++ React18全家桶+Hooks+项目实战-coderwhy2022系统课/05_learn_scaffold/src/App.jsx
@@ -1,9 +1,29 @@
+// css transition动画
+
 import React, { PureComponent } from 'react'
+import { CSSTransition } from 'react-transition-group'
+import './app.css'
 
 export default class App extends PureComponent {
-  render() {
-    return (
-      <div>App</div>
-    )
-  }
+    constructor() {
+        super()
+        this.state = {
+            isShow: true
+        }
+    }
+    render() {
+        const { isShow } = this.state
+        return (
+            <div>
+                <button onClick={e => this.setState({ isShow: !isShow })}>点击切换</button>
+                {/* {
+                    isShow && <h2>hahahahhahah</h2>
+                } */}
+                {/* timeout 里的时间要和css中的时间保持一致；timeout决定类的添加和移除时间 */}
+                <CSSTransition in={isShow} appear classNames='why' timeout={2000} unmountOnExit={true}>
+                    <h2>hahahahhahah</h2>
+                </CSSTransition>
+            </div>
+        )
+    }
 }
