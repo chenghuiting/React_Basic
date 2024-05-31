e9954c7b05360d01ccc25b2231b8bf1020bfb1f0
“chenghuiting” <1365256914@qq.com>
2024-05-31 23:50:02 +0800

    less

--- React18全家桶+Hooks+项目实战-coderwhy2022系统课/05_learn_scaffold/src/App.jsx
+++ React18全家桶+Hooks+项目实战-coderwhy2022系统课/05_learn_scaffold/src/App.jsx
@@ -1,9 +1,13 @@
 import React, { PureComponent } from 'react'
+import './App.less';
 
 export default class App extends PureComponent {
   render() {
     return (
-      <div>App</div>
+      <div>
+        <h2 className='title'>哈哈哈哈哈哈哈哈哈</h2>
+        App
+        </div>
     )
   }
 }
