63edb77eb88a722abe229dc4689f8155f42255c8
“chenghuiting” <1365256914@qq.com>
2024-06-06 15:05:44 +0800

    redux-devtools

--- React18全家桶+Hooks+项目实战-coderwhy2022系统课/07_react_redux/src/store/index.js
+++ React18全家桶+Hooks+项目实战-coderwhy2022系统课/07_react_redux/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { thunk } from 'redux-thunk'
 import reducer from './reducer'
 
@@ -7,8 +7,12 @@ import reducer from './reducer'
     想要派发函数store.dispatch(function)必须要对store进行增强；
 */
 
+// redux-devtools 在开发环境打开，在生产环境不用打开；打包时还需要做是生产环境还是开发环境的判断
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace: true}) || compose; // 开启trace
 
 // applyMiddleware(thunk, xx, xx) 可以传入多个中间键；
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 export default store
