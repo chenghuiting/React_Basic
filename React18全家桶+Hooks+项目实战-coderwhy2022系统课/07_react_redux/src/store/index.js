70127e2d7ebc6726116a84079d998decf18a99bb
“chenghuiting” <1365256914@qq.com>
2024-06-14 11:16:46 +0800

    thunk 库的核心原理

--- React18全家桶+Hooks+项目实战-coderwhy2022系统课/07_react_redux/src/store/index.js
+++ React18全家桶+Hooks+项目实战-coderwhy2022系统课/07_react_redux/src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import { thunk } from 'redux-thunk'
+// import { thunk } from 'redux-thunk'
 import counterReducer from './counter/reducer'
 import homeReducer from './home/reducer'
 
@@ -31,6 +31,42 @@ const reducer = combineReducers({
 //     }
 // }
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+// const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(reducer)
+
+// 实现打印日志的中间件
+// 对每次派发的action进行拦截，进行日志打印
+function log(store) {
+    const next = store.dispatch
+
+    function logAndDispatch(action) {
+        console.log('当前派发的action', action);
+        // 真正派发的代码：使用之前dispatch进行派发；
+        next(action)
+
+        console.log('派发之后的结果', store.getState());
+
+    }
+    /*
+     monkey patch 猴补丁=》篡改现有的代码，对整体的执行程序进行修改； 
+     意味着执行 store.dispatch()时，实际上是在执行logAndDispatch函数；   
+    */
+    store.dispatch = logAndDispatch
+}
+log(store)
+
+// thunk 库的核心原理
+function thunk(store) {
+    const next = store.dispatch
+    function dispatchThunk(action) {
+        if (typeof action === "function") {
+            action(store.dispatch, store.getState)
+        } else {
+            next(action)
+        }
+    }
+    store.dispatch = dispatchThunk
+}
+thunk(store)
 
 export default store
